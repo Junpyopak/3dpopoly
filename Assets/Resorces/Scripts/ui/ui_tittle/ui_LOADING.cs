5f70daba5c146415d5fd689560cf1caab41e8259
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-11-20 22:35:20 +0900

    코르틴으로 정해진 값에서 로딩 멈췄다 재 로딩

--- Assets/Resorces/Scripts/ui/ui_tittle/ui_LOADING.cs
+++ Assets/Resorces/Scripts/ui/ui_tittle/ui_LOADING.cs
@@ -14,26 +14,58 @@ public class ui_LOADING : MonoBehaviour
     public float SceneLoadTime = 0;
     public float LoadingTime = 100f;
     public int StopCount = 5;
+
+    private IEnumerator cortine;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < StopCount; i++)
-        {
-            int random = Random.Range(0, 100);
-            Debug.Log($"{random}");
-            if ((int)slider.value == random)
-            {
-                Time.timeScale = 0;
-            }
-        }
+        
+        //for (int i = 0; i < StopCount; i++)
+        //{
+        //    int random = Random.Range(0, 100);
+        //    Debug.Log($"{random}");
+        //    if ((int)slider.value == random)
+        //    {
+        //        Time.timeScale = 0;
+        //    }
+        //}      
     }
 
     // Update is called once per frame
     void Update()
     {
         Timer();
+        if (SceneLoadTime >= 73)
+        {
+            Pause();
+            cortine = waitTime(5);
+            StartCoroutine(cortine);
+            if (SceneLoadTime >= 20)
+            {
+                StopCoroutine(cortine);
+                StartCoroutine (cortine);
+                if (SceneLoadTime >= 60)
+                {
+                    Pause();
 
+                    if (SceneLoadTime >= LoadingTime)
+                    {
+                        SceneManager.LoadScene("LOBBY");
+                    }
+                }
+            }
+
+        }
     }
+    public IEnumerator waitTime(float wait)
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(wait);
+            Resume();
+        }
+    }
+
 
     private void initSlider()
     {
@@ -44,13 +76,35 @@ public class ui_LOADING : MonoBehaviour
     private void Timer()
     {
 
-        SceneLoadTime += Time.deltaTime *2f;
+        SceneLoadTime += Time.deltaTime * 5f;
         initSlider();
-        if (SceneLoadTime >= LoadingTime)
-        {
-            SceneManager.LoadScene("LOBBY");
-        }
+        //if (SceneLoadTime >= 3)
+        //{
+        //    Pause();
+        //    //StartCoroutine(waitTime(5));
+        //    if (SceneLoadTime >= 63)
+        //    {
+        //        Pause();
+        //        if (SceneLoadTime >= 78)
+        //        {
+        //            Pause();
+        //            if (SceneLoadTime >= LoadingTime)
+        //            {
+        //                SceneManager.LoadScene("LOBBY");
+        //            }
+        //        }
+        //    }
+
+        //}
 
 
+    }
+    private void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    void Resume()
+    {
+        Time.timeScale = 1;
     }
 }
