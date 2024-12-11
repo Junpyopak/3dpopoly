cb1727513292ec352573d800fc353065121ae403
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-12-11 18:30:07 +0900

    체력바 버그

--- Assets/Scripts/Character/Hpbar.cs
+++ Assets/Scripts/Character/Hpbar.cs
@@ -6,28 +6,26 @@ using UnityEngine.EventSystems;
 
 public class Hpbar : MonoBehaviour
 {
-    public float MaxHP;
-    public float currentHP;
+    public float MaxHP = 100;
+    public float currentHP =0;
     public Slider HPBar;
 
     void Start()
     {
-        currentHP = MaxHP;
-        HPBar.maxValue = MaxHP; //slider의 MaxValue를 우리가 원하는 체력 최대치로 초기화
-        HPBar.value = currentHP; //slider의 value 값을 현재의 HP 값으로 초기화
+        HPBar.value = (float)currentHP / (float)MaxHP;
     }
 
     void Update()
     {
-        Damage();
-        HPBar.value = currentHP;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentHP -= 1f;
+        }
+        HpCheck();
     }
 
-    void Damage()
+    void HpCheck()
     {
-        if(Input.GetKey(KeyCode.Space))
-        {
-            currentHP -= 10;
-        }       
+        HPBar.value = currentHP;
     }
 }
