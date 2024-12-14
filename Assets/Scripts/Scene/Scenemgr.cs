3a0a53cb974d29936b2e1554704fecbd4f73da74
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-12-14 23:14:40 +0900

    테이블 정보

--- Assets/Scripts/Scene/Scenemgr.cs
+++ Assets/Scripts/Scene/Scenemgr.cs
@@ -12,7 +12,10 @@ public partial class Scenemgr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shared.InitTableMgr();
+
+        Table_Character.Info info = Shared.TableMgr.Character.Get(1);
+        Table_Item.Info info1 = Shared.TableMgr.Item.Get(1);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Shared.cs
+++ Assets/Scripts/Shared.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public static class Shared 
 {
     public static Scenemgr Scenemgr;//싱글턴 패턴 static 정적으로
+    public static Table_Manager TableMgr;
+
+    public static Table_Manager InitTableMgr()
+    {
+        if (TableMgr == null)
+        {
+            TableMgr = new Table_Manager();
+            TableMgr.Init();
+        }
+
+        return TableMgr;
+    }
 }
