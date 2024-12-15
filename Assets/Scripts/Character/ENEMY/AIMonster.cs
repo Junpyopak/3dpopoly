2699470dada7bc195405e78d221c84bfc270730f
okhrogo <okhrogo@gmail.com>
2024-12-15 13:32:20 +0900

    몬스터 공격시 콜라이더

--- Assets/Scripts/Character/ENEMY/AIMonster.cs
+++ Assets/Scripts/Character/ENEMY/AIMonster.cs
@@ -5,6 +5,12 @@ using static UI_TITLE;
 
 public class AIMonster : Enemy_AI 
 {
+    SphereCollider sphereCollider;
+    private void Start()
+    {
+        sphereCollider = GameObject.Find("LeftHand").GetComponent<SphereCollider>();
+        sphereCollider.enabled = false;
+    }
     private void Update()
     {
         Search();
@@ -22,5 +28,14 @@ public class AIMonster : Enemy_AI
     protected override void Atack()
     {
         base.Atack();
+    }
+    public void StartAttack()
+    {
+        sphereCollider.enabled = true;
+    }
+
+    public void EndAttack()
+    {
+        sphereCollider.enabled = false;
     }
 }
