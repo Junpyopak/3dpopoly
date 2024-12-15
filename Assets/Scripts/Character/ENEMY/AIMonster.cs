72141211cc081135160a4199e13a8354c80da047
okhrogo <okhrogo@gmail.com>
2024-12-15 13:54:37 +0900

    몬스터 어텍 콜라이더오류 해결정

--- Assets/Scripts/Character/ENEMY/AIMonster.cs
+++ Assets/Scripts/Character/ENEMY/AIMonster.cs
@@ -5,12 +5,7 @@ using static UI_TITLE;
 
 public class AIMonster : Enemy_AI 
 {
-    SphereCollider sphereCollider;
-    private void Start()
-    {
-        sphereCollider = GameObject.Find("LeftHand").GetComponent<SphereCollider>();
-        sphereCollider.enabled = false;
-    }
+
     private void Update()
     {
         Search();
@@ -28,14 +23,5 @@ public class AIMonster : Enemy_AI
     protected override void Atack()
     {
         base.Atack();
-    }
-    public void StartAttack()
-    {
-        sphereCollider.enabled = true;
-    }
-
-    public void EndAttack()
-    {
-        sphereCollider.enabled = false;
     }
 }
