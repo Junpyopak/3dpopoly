8b386aadb1f67ca93e6ee500a837eef9afbff1e3
okhrogo <okhrogo@gmail.com>
2024-12-22 15:24:53 +0900

    보스 패턴 구현할것들

---
+++ Assets/Scripts/Character/ENEMY/Boss/BOSS.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOSS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool Attack1;
+    public bool Attack2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Attack()
+    {
+        if(Attack1==true)
+        {
+
+        }
+    }
+}
