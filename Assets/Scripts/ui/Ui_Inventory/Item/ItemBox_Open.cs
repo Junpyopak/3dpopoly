e15cf85230de1e29cc4971490b2d7f780a8dc217
gh1231002 <gh1231002@naver.com>
2025-02-09 15:02:12 +0900

    2/9에러수정중

--- Assets/Scripts/ui/Ui_Inventory/Item/ItemBox_Open.cs
+++ Assets/Scripts/ui/Ui_Inventory/Item/ItemBox_Open.cs
@@ -10,6 +10,7 @@ public class ItemBox_Open : MonoBehaviour
 
     Animator anim;
     bool isPlayerEnter; // Player가 범위 안에 왔는지를 판별할 bool 타입 변수
+    bool boxOpening = false;
     [SerializeField]
     private Text ActionText;
 
@@ -21,19 +22,27 @@ public class ItemBox_Open : MonoBehaviour
         isPlayerEnter = false;
     }
 
+    IEnumerator BoxOpen()
+    {
+        yield return new WaitForSeconds(5.5f);
+        Destroy(gameObject);
+    }
     void Update()
     {
         // 플레이어가 범위 안에 있고 E 키를 누른다면
-        if (isPlayerEnter && Input.GetKeyDown(KeyCode.E))
+        if (boxOpening==false&&isPlayerEnter && Input.GetKeyDown(KeyCode.E))
         {
+            boxOpening = true;
             anim.SetTrigger("Open");
+           StartCoroutine(BoxOpen());
+            ActionText.gameObject.SetActive(false);
         }
 
     }
     // 콜라이더를 가진 객체가 (트리거옵션이 체크된)콜라이더 범위 안으로 들어왔고 그게 플레이어라면 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.gameObject == player&&boxOpening==false)
         {
             isPlayerEnter = true;
             ActionText.gameObject.SetActive(true);
