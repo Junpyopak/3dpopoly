4aa2cb8ac3cf9df29e61153a096326f10deff5d0
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2025-03-06 19:38:32 +0900

    미니맵 따라오는기능

--- Assets/Scripts/ui/Ui_Canera/CopyPosition.cs
+++ Assets/Scripts/ui/Ui_Canera/CopyPosition.cs
@@ -5,17 +5,18 @@ public class CopyPosition : MonoBehaviour
 	[SerializeField]
 	private	bool		x, y, z;	// 이 값이 true이면 target의 좌표, false이면 현재 좌표를 그대로 사용
 	[SerializeField]
-	private	Transform	target;		// 쫓아가야할 대상 Transform
+	private	Transform	target;     // 쫓아가야할 대상 Transform
+    private float FollowSpeed = 3;
 
-	//private void Update()
-	//{
-	//	if ( !target ) return;
+    //private void Update()
+    //{
+    //	if ( !target ) return;
 
-	//	transform.position = new Vector3(
-	//		(x ? target.position.x : transform.position.x),
-	//		(y ? target.position.y : transform.position.y),
-	//		(z ? target.position.z : transform.position.z));
-	//}
+    //	transform.position = new Vector3(
+    //		(x ? target.position.x : transform.position.x),
+    //		(y ? target.position.y : transform.position.y),
+    //		(z ? target.position.z : transform.position.z));
+    //}
     private void FixedUpdate()
     {
         if (!target) return;
@@ -24,6 +25,7 @@ public class CopyPosition : MonoBehaviour
             (x ? target.position.x : transform.position.x),
             (y ? target.position.y : transform.position.y),
             (z ? target.position.z : transform.position.z));
+        
     }
 }
 
