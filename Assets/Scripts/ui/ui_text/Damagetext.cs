ceaefb161e77645fddd3c7ea2048c46111d864eb
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2025-05-31 16:09:00 +0900

    몬스터 데미지 텍스트 시간에 따라 알파값 조정

--- Assets/Scripts/ui/ui_text/Damagetext.cs
+++ Assets/Scripts/ui/ui_text/Damagetext.cs
@@ -7,32 +7,36 @@ using UnityEngine.UI;
 public class Damagetext : MonoBehaviour
 {
     public Text text;
-    public float moveSpeed = 1f;
+    public float moveSpeed = 3f;
     public float lifeTime = 1f;
 
     private float elapsed = 0f;
-    private Transform cam;
+    private RectTransform rect;
+    private Color originalColor;
 
     private void Awake()
     {
         if (text == null)
             text = GetComponentInChildren<Text>();
+        rect = text.GetComponent<RectTransform>();
+        originalColor = text.color;
     }
     void OnEnable()
     {
         elapsed = 0f;
-        cam = Camera.main.transform;
+        text.color = originalColor;
     }
 
     void Update()
     {
-        // 위로 이동
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-
-        // 항상 카메라 방향으로
-        transform.forward = cam.forward;
+        
+        rect.anchoredPosition += Vector2.up * moveSpeed * Time.deltaTime;
 
         elapsed += Time.deltaTime;
+        float alpha = Mathf.Lerp(originalColor.a, 0, elapsed / lifeTime);
+        Color newColor = text.color;
+        newColor.a = alpha;
+        text.color = newColor;
         if (elapsed > lifeTime)
         {
             gameObject.SetActive(false);
