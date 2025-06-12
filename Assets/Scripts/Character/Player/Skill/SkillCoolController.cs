dfa8cf9458f9598e0588685f08833dc47086b793
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2025-06-12 23:33:33 +0900

    플레이어 스킬 2 에님과 쿨타임 까지 연결

--- Assets/Scripts/Character/Player/Skill/SkillCoolController.cs
+++ Assets/Scripts/Character/Player/Skill/SkillCoolController.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class SkillCoolController : MonoBehaviour
 {
     public static SkillCoolController instance;
+
     public Button skillButton;
     public Image cooldownOverlay;
-    public Image Skill2cooldownOverlay;
     public float cooldownTime = 8f;
 
-    private float cooldownTimer = 0f;
-    private bool isCooldown = false;
+    public Button skill2Button;
+    public Image Skill2cooldownOverlay;
+    public float Skill2cooldownTime = 10f;
+
+    private float cooldownTimer1 = 0f;
+    private float cooldownTimer2 = 0f;
+    private bool isCooldown1 = false;
+    private bool isCooldown2 = false;
+
     Animator animator;
     public bool isSkillCasting = false;
+
     private void Awake()
     {
         if (instance != null)
@@ -24,67 +32,93 @@ public class SkillCoolController : MonoBehaviour
         }
         instance = this;
     }
+
     void Start()
     {
         cooldownOverlay.fillAmount = 0f;
         Skill2cooldownOverlay.fillAmount = 0f;
-        SetOverlayAlpha(0f);
-        skillButton.onClick.AddListener(UseSkill);
+        SetOverlayAlpha(cooldownOverlay, 0f);
+        SetOverlayAlpha(Skill2cooldownOverlay, 0f);
+
+        skillButton.onClick.AddListener(UseSkill1);
+        skill2Button.onClick.AddListener(UseSkill2);
+
         animator = GameObject.Find("character").GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K) && !isCooldown)
+        if (Input.GetKeyDown(KeyCode.K) && !isCooldown1)
         {
-            UseSkill();
+            UseSkill1();
         }
-        if(Input.GetKeyDown(KeyCode.L) && !isCooldown)
+
+        if (Input.GetKeyDown(KeyCode.L) && !isCooldown2)
         {
             UseSkill2();
         }
 
-        if (isCooldown)
+        if (isCooldown1)
         {
-            cooldownTimer -= Time.deltaTime;
-            cooldownOverlay.fillAmount = 1f -(cooldownTimer / cooldownTime);
+            cooldownTimer1 -= Time.deltaTime;
+            cooldownOverlay.fillAmount = 1f - (cooldownTimer1 / cooldownTime);
 
-            if (cooldownTimer <= 0f)
+            if (cooldownTimer1 <= 0f)
             {
-                isCooldown = false;
+                isCooldown1 = false;
                 cooldownOverlay.fillAmount = 0f;
-                Skill2cooldownOverlay.fillAmount = 0f;
-                SetOverlayAlpha(0f);
+                SetOverlayAlpha(cooldownOverlay, 0f);
                 skillButton.interactable = true;
+            }
+        }
+
+        if (isCooldown2)
+        {
+            cooldownTimer2 -= Time.deltaTime;
+            Skill2cooldownOverlay.fillAmount = 1f - (cooldownTimer2 / Skill2cooldownTime);
+
+            if (cooldownTimer2 <= 0f)
+            {
+                isCooldown2 = false;
+                Skill2cooldownOverlay.fillAmount = 0f;
+                SetOverlayAlpha(Skill2cooldownOverlay, 0f);
+                skill2Button.interactable = true;
             }
         }
     }
 
-    void UseSkill()
+    void UseSkill1()
     {
-        Debug.Log("스킬 사용!");
+        Debug.Log("Skill 1 사용!");
         animator.SetTrigger("Skill1");
+
         isSkillCasting = true;
-        isCooldown = true;
-        cooldownTimer = cooldownTime;
+        isCooldown1 = true;
+        cooldownTimer1 = cooldownTime;
+
         skillButton.interactable = false;
         cooldownOverlay.fillAmount = 1f;
-        SetOverlayAlpha(0.6f);
-    }
-    void UseSkill2()
-    {
-        animator.SetTrigger("Skill2");
-        isSkillCasting = true;
-        isCooldown = true;
-        cooldownTimer = cooldownTime;
-        Skill2cooldownOverlay.fillAmount = 1f;
+        SetOverlayAlpha(cooldownOverlay, 0.6f);
     }
 
-    void SetOverlayAlpha(float alpha)
+    void UseSkill2()
     {
-        Color color = cooldownOverlay.color;
-        color.a = alpha;
-        cooldownOverlay.color = color;
+        Debug.Log("Skill 2 사용!");
+        animator.SetTrigger("Skill2");
+
+        isSkillCasting = true;
+        isCooldown2 = true;
+        cooldownTimer2 = Skill2cooldownTime;
+
+        skill2Button.interactable = false;
+        Skill2cooldownOverlay.fillAmount = 1f;
+        SetOverlayAlpha(Skill2cooldownOverlay, 0.6f);
     }
-   
+
+    void SetOverlayAlpha(Image overlay, float alpha)
+    {
+        Color color = overlay.color;
+        color.a = alpha;
+        overlay.color = color;
+    }
 }
