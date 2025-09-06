a4ca9762f0e4877692df02becd388399455623b1
park <hjklop1109@naver.com>
2025-09-06 16:19:53 +0900

    포톤 붙이는중

---
+++ Assets/Scripts/ui/Ui_Mgr/NetworkManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public string playerPrefabName = "Player";
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("TestRoom", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
+        PhotonNetwork.Instantiate(playerPrefabName, spawnPos, Quaternion.identity);
+    }
+}
