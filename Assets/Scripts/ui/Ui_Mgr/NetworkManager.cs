fa6e499d1cdf2c2662e2b3d687d11f4276ac6074
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2025-09-17 22:45:15 +0900

    포톤 적용중

--- Assets/Scripts/ui/Ui_Mgr/NetworkManager.cs
+++ Assets/Scripts/ui/Ui_Mgr/NetworkManager.cs
@@ -5,25 +5,44 @@ using Photon.Realtime;
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public string playerPrefabName = "Player";
+    public string roomName = "TestRoom";
+    public Vector3 baseSpawnPos = new Vector3(-58f, 16.4500008f, -294f);
 
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+       
+        if(!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            JoinRoom();
+        }
     }
 
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.JoinLobby();
+        //PhotonNetwork.JoinLobby();
+        JoinRoom();
     }
 
-    public override void OnJoinedLobby()
+    //public override void OnJoinedLobby()
+    //{
+    //    PhotonNetwork.JoinOrCreateRoom("TestRoom", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    //}
+    public void JoinRoom()
     {
-        PhotonNetwork.JoinOrCreateRoom("TestRoom", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
     }
-
     public override void OnJoinedRoom()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
+        //Vector3 spawnPos = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
+        Vector3 spawnPos = new Vector3(
+           Random.Range(baseSpawnPos.x - 2f, baseSpawnPos.x + 2f),
+           baseSpawnPos.y,
+           Random.Range(baseSpawnPos.z - 2f, baseSpawnPos.z + 2f)
+       );
         PhotonNetwork.Instantiate(playerPrefabName, spawnPos, Quaternion.identity);
     }
 }
